3a2a90154e49d742d0f1eca03d871764133fa208
hadber <adber@tuta.io>
2023-05-09 11:21:14 +0200

    ghost can move :)

---
+++ scripts/Player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 200
+const ACCELERATION = 2500
+const FRICTION = 2500
+var networked:bool = false
+@onready var _animated_sprite = $AnimatedSprite2D
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# TODO: make the button for example O hide and show UI, and have a UI with different sliders,
+# toggles and stuff and also a "HOST" button which will open a room for friends to join in :)
+
+func _movement(delta):
+
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_vector = input_vector.normalized()
+	
+	for key in ["right", "left", "down", "up"]:
+		if Input.is_action_pressed(key):
+			_animated_sprite.play("walk_"+key)
+	
+	if input_vector != Vector2.ZERO:
+#		animationPlayer.play("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+#		animationPlayer.play("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	move_and_slide() 
+#	add_player_state()
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	move_and_slide()
+	_movement(delta)
---
+++ scripts/gWorld.gd
@@ -0,0 +1,33 @@
+extends Node
+
+const PlayerScene = preload("res://scenes/Player.tscn")
+var Player1
+var spawn_side:Vector2 = Vector2(450, 260)
+var last_world_state = 0
+var PlayerState:Dictionary = {}
+var remotePlayers:Array = []
+var currentRoom
+
+func _ready():
+	Player1 = PlayerScene.instance()
+	Player1.get_node("CenterContainer/Name").text = Steam.getFriendPersonaName(Global.gSteamID)
+
+func add_remote_player(pSteamID:String, pPos:Vector2):
+	var remotePlayer = PlayerScene.instance()
+	if not remotePlayers.has(pSteamID):
+		remotePlayer.networked = true
+		remotePlayer.get_node("CenterContainer/Name").text = Steam.getFriendPersonaName(int(pSteamID))
+		remotePlayer.name = pSteamID
+		remotePlayer.spawn_me(pPos)
+		currentRoom.get_node("Entities").add_child(remotePlayer)
+		remotePlayers.append(remotePlayer)
+
+func remove_remote_player(pSteamID:String):
+	for player in remotePlayers:
+		if player.name == pSteamID:
+			remotePlayers.erase(player)
+			if currentRoom.get_node("Entities").has_node(pSteamID):
+				currentRoom.get_node("Entities").get_node(pSteamID).queue_free()
+
+func add_player_state(pState:Dictionary):
+	PlayerState = pState
